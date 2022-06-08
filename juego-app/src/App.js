6ec1edec88a3e8a3e37f1c41ec4884ff2af2be0d
TalysJavier <talysjavier@gmail.com>
2022-06-07 21:37:25 -0400

    Agredao ItemCount

--- juego-app/src/App.js
+++ juego-app/src/App.js
@@ -1,23 +1,25 @@
-
 import './App.css';
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
 
+function App() {
 
-const App = () => {
-  return (
-    
-    <div className="App">
-
-        <NavBar/>
-        <ItemListContainer texto={"Hola bienvenidos"}/>
-        <x/>
-
-        
-
+  const onAdd =(quantity)=>{
+    alert(`Tu compra en Bmw Mundo es de ${quantity} unidades`);
+  }
+  
+  
+    return ( 
+    <div className = "App" >
+      
+      
+    <NavBar/>
+    <ItemListContainer texto={"Hola bienvenidos a Bmw Mundo"}/>
+    <ItemCount initial={1} stock ={5} onAdd={onAdd}/>
     </div>
-  );
+    );
 }
 
 export default App;
